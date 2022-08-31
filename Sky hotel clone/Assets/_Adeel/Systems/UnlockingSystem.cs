ac79b7819b0a464c271d43a98bf834d62b8ccba4
adeelwitty <adeelwitty@gmail.com>
2022-08-31 16:12:19 +0500

    Changes

--- Sky hotel clone/Assets/_Adeel/Systems/UnlockingSystem.cs
+++ Sky hotel clone/Assets/_Adeel/Systems/UnlockingSystem.cs
@@ -9,14 +9,12 @@
 // </author>
 // _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_
 
-using System;
 using System.Collections;
 using _Adeel.Managers;
 using _Adeel.Player;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace _Adeel.Systems
 {
@@ -31,8 +29,10 @@ namespace _Adeel.Systems
         [SerializeField] private float unlockIterationDelay = 0.1f, trigStayDelay = 0.5f;
         private bool isUnlocked, isInTrigArea;
 
-        [Range(0, 1)] [SerializeField] private float testProgress; 
-        
+        [SerializeField] private Collider trigCollider;
+
+        // [Range(0, 1)][SerializeField] private float testProgress;
+
         private static readonly int Arc1 = Shader.PropertyToID("_Arc1");
 
         void Start()
@@ -40,27 +40,29 @@ namespace _Adeel.Systems
             isUnlocked = PlayerPrefs.GetInt(prefKey, 0) > 0;
             if (isUnlocked)
             {
-                UnlockItem();
+                Unlocked();
             }
-    
-            unlockRemainPrice = PlayerPrefs.GetInt(prefKey + "_remainPrice", unlockPrice);
-            txtUnlockPrice.text = unlockRemainPrice.ToString("00");
-            UnlockProgress();
+            else
+            {
+                unlockRemainPrice = PlayerPrefs.GetInt(prefKey + "_remainPrice", unlockPrice);
+                txtUnlockPrice.text = unlockRemainPrice.ToString("00");
+                UnlockProgress();
+            }
         }
 
         private void OnValidate()
         {
-            progressBar.sharedMaterial.SetFloat( Arc1, 360f - testProgress * 360f);
+            // progressBar.material.SetFloat(Arc1, 360f - testProgress * 360f);
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (!other.TryGetComponent(out PlayerMovement playerMovement) || isUnlocked) return;
-    
+
             isInTrigArea = true;
             DOVirtual.DelayedCall(trigStayDelay, () => StartCoroutine(CO_Unlock(unlockIterationDelay)));
         }
-    
+
         void OnTriggerExit(Collider other)
         {
             if (other.TryGetComponent(out PlayerMovement playerMovement))
@@ -68,22 +70,22 @@ namespace _Adeel.Systems
                 isInTrigArea = false;
             }
         }
-    
+
         IEnumerator CO_Unlock(float delay)
         {
             while (isInTrigArea)
             {
                 yield return new WaitForSeconds(delay);
-    
+
                 if (ScoreManager.Ins.Score > 0)
                 {
                     unlockRemainPrice--;
                     ScoreManager.Ins.Score--;
-    
+
                     PlayerPrefs.SetInt(prefKey + "_remainPrice", unlockRemainPrice);
                     UnlockProgress();
                     txtUnlockPrice.text = unlockRemainPrice.ToString("00");
-    
+
                     if (unlockRemainPrice <= 0)
                     {
                         UnlockItem();
@@ -93,21 +95,29 @@ namespace _Adeel.Systems
             }
             yield return null;
         }
-    
-    
-    
+
+
+
         private void UnlockProgress()
         {
-            float progress = Mathf.Abs(1f - (float) unlockRemainPrice / (float) unlockPrice);
-            progressBar.sharedMaterial.SetFloat( Arc1, 360f - progress * 360f);
-            // return Mathf.Abs(1f - (float)unlockRemainPrice / (float)unlockPrice);
+            float progress = Mathf.Abs(1f - (float)unlockRemainPrice / (float)unlockPrice);
+            progressBar.material.SetFloat(Arc1, 360f - progress * 360f);
         }
-    
+
+        public void Unlocked()
+        {
+            unlockProgressObj.SetActive(false);
+            unlockItemPrefab.SetActive(true);
+            if (trigCollider != null)
+            {
+                trigCollider.enabled = false;
+            }
+        }
+
         public void UnlockItem()
         {
             PlayerPrefs.SetInt(prefKey, 1);
-            unlockProgressObj.SetActive(false);
-            unlockItemPrefab.SetActive(true);
+            Unlocked();
             // GameObject item = Instantiate(unlockItemPrefab, transform.position + (Vector3.up * 0.25f), Quaternion.identity);
             unlockItemPrefab.transform.DOScale(0f, 0.5f).From().SetEase(Ease.OutBounce).SetDelay(0.25f);
         }
